c105b40bf5b2acf0759dc082d25c2092e18c7709
baron159 <seb8653@g.rit.edu>
2015-02-12 00:01:02 -0500

    Lesson 20 is done now

--- Lesson 19.py
+++ Lesson 19.py
@@ -1,12 +1,12 @@
 __author__ = 'Scott Baron'
 
- # this is an example for set in Python
+# this is an example for set in Python
 
 numSet = {9, 5, 7, 10, 8, 6, 5, 74, 10, 8}
 numList = [9, 5, 7, 10, 8, 6, 5, 74, 10, 8]
 
 
- # Set can cantain only unique entries, this is testing the if a repeat entry is counted in the overall size of the set
+# Set can cantain only unique entries, this is testing the if a repeat entry is counted in the overall size of the set
 n = 0
 for y in numSet:
     n += 1
--- Lesson 20.py
+++ Lesson 20.py
@@ -4,4 +4,9 @@ __author__ = 'Scott'
 
 gameDic = {1: "Civ 5", 2: "LOL", 3: "DL", 4: "Halo", 5: "Evolve"}
 
-print(gameDic.items())
+# This will only return one element 2nd
+print(gameDic.get(2))
+print('\n')
+
+for k, v in gameDic.items():
+    print(k, v)
