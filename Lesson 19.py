3c201d52a4b408d2a5674a59a776dc350278d19d
Scott <baron159@live.com>
2015-02-11 19:10:20 -0500

    Lesson 19 done Lesson 20 Started

--- Lesson 19.py
+++ Lesson 19.py
@@ -1,3 +1,20 @@
 __author__ = 'Scott Baron'
 
+ # this is an example for set in Python
 
+numSet = {9, 5, 7, 10, 8, 6, 5, 74, 10, 8}
+numList = [9, 5, 7, 10, 8, 6, 5, 74, 10, 8]
+
+
+ # Set can cantain only unique entries, this is testing the if a repeat entry is counted in the overall size of the set
+n = 0
+for y in numSet:
+    n += 1
+print(n)
+
+n = 0
+for y in numList:
+    n += 1
+print('\n', n)
+
+# Conclusion - repeated items are not included in the overall size of a Set
---
+++ Lesson 20.py
@@ -0,0 +1,7 @@
+__author__ = 'Scott'
+
+# This is an example of Dictanrys similar to Hash tables in java
+
+gameDic = {1: "Civ 5", 2: "LOL", 3: "DL", 4: "Halo", 5: "Evolve"}
+
+print(gameDic.items())
